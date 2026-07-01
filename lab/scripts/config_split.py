ecdcb7a101b8569d2ad2c1beaf88367e14e2a52d
fmarzo <fremarzo94@gmail.com>
2026-07-01 19:26:06 +0200

    feat: added configuration file for label dataset file

---
+++ lab/scripts/config_split.py
@@ -0,0 +1,6 @@
+
+DATASET_ROOT = '/work/cvcs2026/latent_space_cowboys/datasets/HemoSet'
+OUT_DIR_SPLIT = '../splits'
+CSV_FILE_NAME = '../splits/dataset.csv'
+NUM_DATASET_FOLDERS = 11
+IMG_EXT = '.png'
