972f532f535602cdc0f25d072e55c884151f9493
fmarzo <fremarzo94@gmail.com>
2026-07-09 21:05:49 +0200

    feat: first implementation of unet. Modified bash to launch a single GPU node

--- lab/scripts/train.py
+++ lab/scripts/train.py
@@ -10,12 +10,12 @@ brief:  this script acts as "main" entry for python calls
 
 import numpy as np
 from PIL import Image
-import matplotlib.pyplot as plt
 import torch
 import torchvision.transforms as transforms
 from src.hemoset_dataset import CustomImageDataset
 from src import config_split
 from torch.utils.data import DataLoader
+import segmentation_models_pytorch as smp
 
 """
 function: tensor_proprieties
@@ -24,7 +24,6 @@ brief:    this routine displays all the useful proprieties of a torch tensor (sh
 def tensor_proprieties (tensor, index):
     print(f"shape: {tensor[index].shape}")
     print(f"type: {tensor[index].dtype}")
-    print(f"shape {tensor[index].shape}")
     print(f"values: {torch.unique(tensor[index])}")
     print(f"len: {len(tensor[index])}")
 
@@ -46,9 +45,38 @@ train_hemo_DL = DataLoader(train_ds, 4, num_workers=2, shuffle=True)
 valid_hemo_DL = DataLoader(valid_ds, 4, num_workers=2, shuffle=False)
 test_hemo_DL  = DataLoader(test_ds,  4, num_workers=2, shuffle=False)
 
-train_features, train_labels = next(iter(train_hemo_DL))
-print(f"Feature batch shape: {train_features.size()}")
-print(f"Labels batch shape: {train_labels.size()}")
+train_img, train_mask = next(iter(train_hemo_DL))
+print(f"Feature batch shape: {train_img.size()}")
+print(f"Labels batch shape: {train_mask.size()}")
 
+# instantiate the unet
 
+unet = smp.Unet(encoder_name="resnet18", encoder_weights="imagenet", in_channels=3, classes=2)
+unet.to("cuda")
 
+# images 
+train_img = train_img.to("cuda")
+img_1 = unet(train_img)
+print(img_1.shape)
+
+# mask
+# uses index to avoid removal batch size of 1
+train_mask = torch.squeeze(train_mask,1).to(torch.long).to("cuda")
+
+# loss
+ce_loss = torch.nn.CrossEntropyLoss().to("cuda")
+print(ce_loss(img_1, train_mask))
+
+# optimizer Adam
+
+adam = torch.optim.Adam(unet.parameters(), lr=0.001)
+
+#testing overfit on the same batch (4): does the model learn?
+for i in range(300):
+    adam.zero_grad()
+    img_1 = unet(train_img)
+    loss = ce_loss(img_1, train_mask)
+    if i % 50 == 0:
+        print(loss)
+    loss.backward()
+    adam.step()
