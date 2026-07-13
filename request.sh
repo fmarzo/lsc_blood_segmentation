19d3aedddb1f2795403c77fc284898f899347228
Filippo Giusti <fgiusti48@gmail.com>
2026-07-13 16:39:03 +0200

    chore: modified request.sh

--- request.sh
+++ request.sh
@@ -4,18 +4,16 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
-#SBATCH --time=00:60:00
-#SBATCH --output=/work/cvcs2026/latent_space_cowboys/logs/train_%j.txt
-#SBATCH --error=/work/cvcs2026/latent_space_cowboys/logs/train_%j.err
+#SBATCH --time=01:00:00
+#SBATCH --output=/work/cvcs2026/latent_space_cowboys/logs/train_%j.log
+#SBATCH --error=/work/cvcs2026/latent_space_cowboys/logs/train_%j.log
 #SBATCH --account=cvcs2026
-
 
 source /homes/$USER/cvcs2026/venv/bin/activate
 
-echo "launching the python script.." 
+echo "Launching the Python script..."
 
-# python ./lsc_blood_segmentation/lab/scripts/train.py --config config.yaml
-cd ./lab
+cd ./lab || exit 1
 python -m scripts.train
 
-echo "launching the python script.."
+echo "Python script completed."
