f498a6c7cf466b48fa34cfb5b66f3d9bd0107e8f
fmarzo <fremarzo94@gmail.com>
2026-07-15 21:10:14 +0200

    chore: modified constraints on sbatch

--- request.sh
+++ request.sh
@@ -4,10 +4,11 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
-#SBATCH --time=03:00:00
+#SBATCH --time=00:30:00
 #SBATCH --output=/work/cvcs2026/latent_space_cowboys/logs/train_%j.log
 #SBATCH --error=/work/cvcs2026/latent_space_cowboys/logs/train_%j.log
 #SBATCH --account=cvcs2026
+#SBATCH --constraint="gpu_2080_11G|gpu_A40_45G|gpu_K80_12G|gpu_L40S_45G|gpu_RTX5000_16G|gpu_RTX6000_24G|gpu_RTX_A5000_24G"
 
 source /homes/$USER/cvcs2026/venv/bin/activate
 
